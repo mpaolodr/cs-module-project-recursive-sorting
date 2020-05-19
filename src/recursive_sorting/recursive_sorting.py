770165624ce22905b78d82b7de717686442dc775
mpaolodr <mpaolodr_1504@yahoo.com>
2020-05-19 19:23:53 -0400

    tests passing for both merge_sort and merge_sort_in_place

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -5,6 +5,19 @@ def merge(arrA, arrB):
 
     # Your code here
 
+    for i in range(len(merged_arr)):
+
+        if len(arrA) > 0 and len(arrB) > 0:
+            if arrA[0] > arrB[0]:
+                merged_arr[i] = arrB.pop(0)
+            else:
+                merged_arr[i] = arrA.pop(0)
+        else:
+
+            if len(arrA) == 0:
+                merged_arr[i] = arrB.pop(0)
+            else:
+                merged_arr[i] = arrA.pop(0)
 
     return merged_arr
 
@@ -13,6 +26,21 @@ def merge(arrA, arrB):
 def merge_sort(arr):
     # Your code here
 
+    if len(arr) == 0:
+        return arr
+
+    if len(arr) == 1:
+        return arr
+
+    else:
+        mid = len(arr) // 2
+
+        arrA = merge_sort(arr[mid:])
+        arrB = merge_sort(arr[:mid])
+
+        sorted_arr = merge(arrA, arrB)
+
+        return sorted_arr
 
     return arr
 
@@ -21,6 +49,28 @@ def merge_sort(arr):
 def merge_in_place(arr, start, mid, end):
     # Your code here
 
+    start2 = mid + 1
+
+    if arr[mid] <= arr[start2]:
+        return arr
+
+    while start <= mid and start2 <= end:
+        if arr[start] <= arr[start2]:
+            start += 1
+
+        else:
+            value = arr[start2]
+            index = start2
+
+            while index != start:
+                arr[index] = arr[index - 1]
+                index -= 1
+
+            arr[start] = value
+
+            start += 1
+            mid += 1
+            start2 += 1
 
     return arr
 
@@ -28,8 +78,19 @@ def merge_in_place(arr, start, mid, end):
 def merge_sort_in_place(arr, l, r):
     # Your code here
 
+    if len(arr) <= 1:
+        return arr
 
-    return arr
+    if l < r:
+
+        mid = (l + r) // 2
+
+        merge_sort_in_place(arr, l, mid)
+        merge_sort_in_place(arr, mid + 1, r)
+
+        sorted_arr = merge_in_place(arr, l, mid, r)
+
+        return sorted_arr
 
 
 # STRETCH: implement the Timsort function below
